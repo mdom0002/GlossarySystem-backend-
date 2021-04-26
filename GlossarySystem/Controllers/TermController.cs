1726845f1d0a2ab66be946becbd72cc110efc2b7
medo <mdom0002student.monash.edu>
2021-04-26 10:46:18 +0530

    first commit

---
+++ GlossarySystem/Controllers/TermController.cs
@@ -0,0 +1,127 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GlossarySystem.Models;
+
+namespace GlossarySystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TermController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public TermController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"
+                    select * FROM master.DBO.TERM";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("TermAppCon");
+            SqlDataReader myReader;
+            using(SqlConnection myCon=new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query,myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader); 
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult(table);
+        }
+
+        [HttpPost]
+        public JsonResult Post(TERM term)
+        {
+            string query = @"
+                    insert into master.DBO.TERM
+                    (WORD,DEF)
+                    values 
+                    (
+                    '" + term.WORD + @"'
+                    ,'" + term.DEF + @"'
+                    )
+                    ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("TermAppCon");
+            SqlDataReader myReader;
+            using(SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader); ;
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Added Successfully");
+        }
+
+        [HttpPut]
+        public JsonResult Put(TERM term)
+        {
+            string query = @"
+                    update master.DBO.TERM set DEF = '"+term.DEF+@"'
+                    where TERMID = "+term.TERMID+ @"
+                ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("TermAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader); ;
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Updated Successfully");
+        }
+
+        [HttpDelete("{termid}")]
+        public JsonResult Delete(int termid)
+        {
+            string query = @"
+                    delete from master.DBO.TERM where TERMID = " + termid + @"
+                ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("TermAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader); ;
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Deleted Successfully");
+        }
+    }
+}
---
+++ GlossarySystem/Models/Term.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GlossarySystem.Models
+{
+    public class TERM
+    {
+        public int TERMID { get; set; }
+        public string WORD { get; set; }
+        public string DEF { get; set; }
+    }
+}
